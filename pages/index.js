86e44587d2db1d13b07319bab6e0c2466666dab8
Shounak2003 <ss4958@srmist.edu.in>
2024-01-02 17:10:34 +0530

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -19,15 +19,17 @@ const Home = () => {
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10 ">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* title */}
-          <motion.h1 
-            variants={fadeIn('down', 0.2)} initial="hidden" animate="show" exit="hidden" 
-            className='h1'>
-            <span className='text-white'>A Code Composer :</span><br /> Building The Next
-            <span className='text-accent'>
-              <br />
-              Digital Frontiers
-            </span>
-          </motion.h1>
+{/* title */}
+<motion.h1 
+  variants={fadeIn('down', 0.2)} initial="hidden" animate="show" exit="hidden" 
+  className='h1'>
+  <span className='text-white'>A Code Composer :</span><br /> Building The Next{' '}
+  <span className='text-accent'>
+    <br />
+    Digital Frontiers
+  </span>
+</motion.h1>
+
           {/* subtitle */}
           <motion.p 
             variants={fadeIn('down', 0.3)} initial="hidden" animate="show" exit="hidden" 
