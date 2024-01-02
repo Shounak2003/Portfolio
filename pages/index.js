5c5c6d66d142dd1dac10d4a625ddf998fa06a3cc
Shounak2003 <ss4958@srmist.edu.in>
2024-01-02 17:35:47 +0530

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 //next image
-import image from "next/image";
+import Image from "next/image";
+import Head from 'next/head'; // Import Head from next/head
 
 //components
 import ParticlesContainer from "../components/ParticlesContainer";
@@ -7,29 +8,32 @@ import ProjectsBtn from "../components/ProjectsBtn";
 import Avatar from "../components/Avatar";
 
 //framer motion
-import {motion} from 'framer-motion'; 
+import { motion } from 'framer-motion'; 
 
 //variants
-import {fadeIn} from '../variants';
+import { fadeIn } from '../variants';
 
 const Home = () => {
   return (
     <div className='bg-primary/60 h-full'>
+      {/* Add Head component for custom title */}
+      <Head>
+        <title>Shounak&rsquo;s{' '} Portfolio</title>
+      </Head>
+
       {/* text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10 ">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* title */}
-{/* title */}
-<motion.h1 
-  variants={fadeIn('down', 0.2)} initial="hidden" animate="show" exit="hidden" 
-  className='h1'>
-  <span className='text-white'>A Code Composer :</span><br /> Building Tomorrows
-
-  <span className='text-accent'>
-    <br />
-    Digital Frontiers
-  </span>
-</motion.h1>
+          <motion.h1 
+            variants={fadeIn('down', 0.2)} initial="hidden" animate="show" exit="hidden" 
+            className='h1'>
+            <span className='text-white'>A Code Composer :</span><br /> Building Tomorrow&rsquo;s{' '}
+            <span className='text-accent'>
+              <br />
+              Digital Frontiers
+            </span>
+          </motion.h1>
 
           {/* subtitle */}
           <motion.p 
