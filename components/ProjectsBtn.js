cce8f3523c6faf5c41fd9ee8002319ebb253e18a
Shounak2003 <ss4958@srmist.edu.in>
2024-01-02 18:12:30 +0530

    modified:   components/ProjectsBtn.js         new file:   public/resume.pdf         modified:   styles/globals.css

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,20 +1,45 @@
-//next image
+// next/image
 import Image from "next/image";
 
-//next link
+// next/link
 import Link from "next/link";
 
-//icons
-import  {HiArrowRight} from 'react-icons/hi2'
+// react-icons
+import { HiArrowRight } from "react-icons/hi2";
 
 const ProjectsBtn = () => {
-  return <div className="mx-auto xl:mx-0">
-    <Link href={'/work'} 
-      className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-no-repeat group">
-      <Image src={ '/rounded-text.png '} width={141} height={148} alt='' className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"/>
-      <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300"/>
-    </Link>
-    </div>;
+  const openCVInNewTab = () => {
+    // Update the file path if needed
+    const cvFilePath = '/resume.pdf';
+
+    // Open the CV in a new tab
+    window.open(cvFilePath, '_blank');
+  };
+
+  return (
+    <div className="mx-auto xl:mx-0 text-white">
+      <div
+        className="relative w-[185px] h-[185px] flex flex-col justify-center items-center bg-circleStar bg-cover bg-no-repeat group"
+        onClick={openCVInNewTab}
+        role="button"
+      >
+        {/* Rounded Text Above the Arrow */}
+        
+        
+        {/* <Image
+          src={'/rounded-text.png'}
+          width={141}
+          height={148}
+          alt=""
+          className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
+        /> */}
+        <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300" />
+
+        {/* Rounded Text Below the Arrow */}
+        <div className="text-sm text-center  mt-12  w-full h-full group-hover:translate-x-2 transition-all">Check my CV </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectsBtn;
